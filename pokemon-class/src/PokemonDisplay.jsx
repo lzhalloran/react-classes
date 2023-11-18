b3a402c25c8b02c72b6dfc9b5647db2611cdfd8f
lzhalloran <lzhalloran@gmail.com>
2023-11-18 12:02:29 +1000

    connected pokeAPI

--- pokemon-class/src/PokemonDisplay.jsx
+++ pokemon-class/src/PokemonDisplay.jsx
@@ -3,14 +3,42 @@ import React from "react";
 export default class PokemonDisplay extends React.Component {
   constructor(props) {
     super(props);
-
     // state stores data per component
     this.state = {
       pokemonName: null,
     };
   }
+  async componentDidMount() {
+    console.log("Did mount triggers after the component was rendered");
+    function getRandomPokemonId() {
+      return Math.floor(Math.random() * 1017) + 1;
+    }
+
+    let response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId()
+    ).catch((error) => {
+      throw new Error("API failure");
+    });
+
+    if (response.status === "404") {
+      throw new Error("API did not have data for the requested ID");
+    }
+    //Convert the response into usable JSON
+    let data = await response.json().catch((error) => {
+      throw new Error("API did not return valid JSON");
+    });
+    this.setState({ pokemonName: data.name });
+  }
+
+  componentWillUnmount() {
+    console.log("Will unmount triggers after component is unmounted");
+  }
 
   render() {
-    return <h2>Pokemon name</h2>;
+    if (this.state.pokemonName) {
+      return <h2>{this.state.pokemonName}</h2>;
+    } else {
+      return <h2>Loading...</h2>;
+    }
   }
 }
